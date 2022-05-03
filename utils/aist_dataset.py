edd5d6917294a2a54938988f480c01ecb5ed6583
Eashan Gupta <gupta.eashan@gmail.com>
2022-05-02 20:03:42 -0500

    updated dataset to use preprocessed data

--- utils/aist_dataset.py
+++ utils/aist_dataset.py
@@ -6,7 +6,6 @@ import scipy.io as sio
 # from utils import data_utils
 from matplotlib import pyplot as plt
 import torch
-from tfrecord.torch.dataset import TFRecordDataset
 
 import os 
 
@@ -18,48 +17,25 @@ https://github.com/wei-mao-2019/HisRepItself/blob/master/utils/h36motion3d.py
 
 class Datasets(Dataset):
 
-    def __init__(self, data_dir, input_n=20, output_n=40, skip_rate=40, sample_rate=2, prefix="train", num_files=None):
-        self.path_to_data = data_dir
+    def __init__(self, p3d_pth, audio_pth, input_n=20, output_n=40, skip_rate=40, sample_rate=2, num_files=None):
         self.in_n = input_n*sample_rate
         self.out_n = output_n*sample_rate
         self.sample_rate = sample_rate
-        self.p3d = {}
-        self.audio = {}
+        self.p3d = torch.load(p3d_pth)
+        self.audio = torch.load(audio_pth)
         self.data_idx = []
         seq_len = self.in_n + self.out_n
 
-        file_idx=0
-        key=0
-        for filename in os.listdir(self.path_to_data):
-            if num_files and file_idx>=num_files:
+        file_idx = 0
+        for key in self.p3d.keys():
+            if file_idx >= num_files:
                 break
-            f = os.path.join(self.path_to_data, filename)
-            if os.path.isfile(f) and prefix in f:
-                print("Processing %s" % filename)
-            else:
-                # print("Skipping %s" % filename)
-                continue
             file_idx += 1
-            dataset = TFRecordDataset(f, index_path=None, description=None)
-            loader = torch.utils.data.DataLoader(dataset, batch_size=1)
+            p3d_data = self.p3d[key]
+            nfs = p3d_data.shape[0]
 
-            for i, data in enumerate(loader):
-                pn, pd = data['motion_sequence_shape'][0]
-                pn=pn.item()
-                pd=pd.item()
-                self.p3d[key] = data['motion_sequence'].view((pn, pd))
-                an, ad = data['audio_sequence_shape'][0]
-                an=an.item()
-                ad=ad.item()
-                self.audio[key] = data['audio_sequence'].view((an, ad))
-
-                start_frames = np.arange(0, pn-seq_len+1, skip_rate)
-                self.data_idx = self.data_idx + [(key, sf) for sf in start_frames]
-
-                assert (start_frames<self.p3d[key].shape[0]).all()
-                # print(key, start_frames, self.p3d[key].shape)
-
-                key=key+1
+            start_frames = np.arange(0, nfs-seq_len+1, skip_rate)
+            self.data_idx = self.data_idx + [(key, sf) for sf in start_frames]
 
     def __len__(self):
         return np.shape(self.data_idx)[0]
@@ -68,16 +44,15 @@ class Datasets(Dataset):
         key, start_frame = self.data_idx[item]
 
         fs = np.arange(start_frame, start_frame + self.in_n + self.out_n, self.sample_rate)
-        # ps = np.arange(start_frame + self.in_n, start_frame + self.in_n + self.out_n, self.sample_rate)
 
-        # print(fs[0], ps[-1], self.p3d[key].shape, key, start_frame)
-
-        # return self.p3d[key][fs], self.audio[key][fs], self.p3d[key][ps], self.audio[key][ps]
-        return self.p3d[key][fs], self.audio[key][fs]
+        return torch.concat((torch.zeros(fs.shape[0], 6), self.p3d[key][fs]), axis=1), self.audio[key][fs]
 
 if __name__ == '__main__':
     print("Testing AIST++ dataset")
-    ds = Datasets("/Users/eash/UIUC/CS 598- Vision/project/tf_sstables/")
+    ds = Datasets(
+        p3d_pth="/Users/eash/UIUC/CS 598- Vision/project/efficient_vision_project/data/p3d_train.pth",
+        audio_pth="/Users/eash/UIUC/CS 598- Vision/project/efficient_vision_project/data/audio_train.pth",
+        num_files=1)
 
     dataloader = DataLoader(ds, num_workers=1, batch_size=7, shuffle=True)
     example_batch = next(iter(dataloader))
