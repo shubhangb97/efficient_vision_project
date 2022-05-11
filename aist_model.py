1ce70e72bcf6ad1f464670401a5d3ee4fdf3a3dd
Shubhang Bhatnagar <shubhangb97@gmail.com>
2022-05-11 08:31:01 +0530

    adding cnn to model

--- aist_model.py
+++ aist_model.py
@@ -278,6 +278,100 @@ class Model(nn.Module):
         # batch size, seq_length
         return x
 
+class Model_cnn(nn.Module):
+    """
+    Shape:
+        - Input[0]: Input sequence in :math:`(N, in_channels,T_in, V)` format
+        - Output[0]: Output sequence in :math:`(N,T_out,in_channels, V)` format
+        where
+            :math:`N` is a batch size,
+            :math:`T_{in}/T_{out}` is a length of input/output sequence,
+            :math:`V` is the number of graph nodes.
+            :in_channels=number of channels for the coordiantes(default=3)
+            +
+    """
+
+    def __init__(self,
+                 input_channels,
+                 input_time_frame,
+                 output_time_frame,
+                 st_gcnn_dropout,
+                 joints_to_consider, music_dim,
+                 txc_dropout,step_size, output_step_size,
+                 music_as_joint,
+                 num_layers,txc_kernel_size,
+                 bidirectional=False,
+                 bias=True):
+
+        super(Model,self).__init__()
+        self.input_time_frame=input_time_frame
+        self.output_time_frame=output_time_frame
+        self.joints_to_consider=joints_to_consider
+        self.output_step_size = output_step_size
+        self.input_channels = input_channels
+        self.music_as_joint = music_as_joint
+        self.music_dim = music_dim
+        self.st_gcnns=nn.ModuleList()
+        self.txcnns=nn.ModuleList()
+
+        self.audio_cnn = CNN_layer(music_dim,64,[3,1],txc_dropout)
+
+        self.st_gcnns.append(ST_GCNN_layer(input_channels,64,[1,1],1,input_time_frame,
+                                           joints_to_consider,st_gcnn_dropout))
+        self.st_gcnns.append(ST_GCNN_layer(64,32,[1,1],1,input_time_frame,
+                                               joints_to_consider+music_as_joint,st_gcnn_dropout))
+
+        self.st_gcnns.append(ST_GCNN_layer(32,64,[1,1],1,input_time_frame,
+                                               joints_to_consider+music_as_joint,st_gcnn_dropout))
+
+        self.st_gcnns.append(ST_GCNN_layer(64,input_channels,[1,1],1,input_time_frame,
+                                               joints_to_consider+music_as_joint,st_gcnn_dropout))
+
+        self.rnn_audio_cnn = CNN_layer(music_dim,64,[step_size,1],txc_dropout)#step_size_removed
+
+                # at this point, we must permute the dimensions of the gcn network, from (N,C,T,V) into (N,T,C,V)
+        self.txcnns.append(CNN_layer(input_time_frame,output_time_frame,txc_kernel_size,txc_dropout)) # with kernel_size[3,3] the dimensinons of C,V will be maintained
+        for i in range(1,n_txcnn_layers):
+            self.txcnns.append(CNN_layer(output_time_frame,output_time_frame,txc_kernel_size,txc_dropout))
+
+    def forward(self, x, x_audio, x_audio_future):
+        num_gcn = 0
+        for gcn in (self.st_gcnns):
+            if(num_gcn == 0):
+                #x_audio shape assumes is N*num_music_dim*num_time_frame*1
+                x_audio = self.audio_cnn(x_audio)
+            elif(num_gcn == 1):
+            #x audio here is N*64*num_time_frame*1
+            #x is N*64*num_time_frame*25
+
+                x = torch.cat((x,x_audio.repeat(1,1,1,self.music_as_joint)), dim = 3)
+            x = gcn(x)
+            num_gcn = num_gcn+1
+        #x is N*9*num_time_frame*26
+        x= x.permute(0,2,1,3)
+        #x is N*num_input_time_frame*9*26
+        x = x.reshape((1, x.shape[0], -1))
+        x = x.repeat(self.num_layers_rnn*self.D, 1, 1)
+
+        # x_future  N*num_music_dim*num_output_time_frame*1
+        #breakpoint()
+        x_audio_future = self.rnn_audio_cnn(x_audio_future)
+        seq_length = int(self.output_time_frame / self.output_step_size)
+        # x future is now N*64*num_output_time_frame*1
+        #breakpoint()
+        x_audio_future = x_audio_future.permute(0,2,1,3)
+        # x future is now N*num_output_time_frame*64*1
+        x_audio_future = x_audio_future.reshape((x_audio_future.shape[0],seq_length,-1))
+        #breakpoint()
+        x,_ = self.rnn(x_audio_future, x)
+        #output is to be reshaped to as this shape was input x is N*num_time_frame*9*26
+        x = x.reshape((x.shape[0],seq_length,self.input_time_frame,self.input_channels,self.joints_to_consider+self.music_as_joint))
+        x = x[:,:,self.input_time_frame-self.output_step_size:,:,0:-self.music_as_joint]
+        #breakpoint()
+        x = x.reshape(x.shape[0], x.shape[1]*x.shape[2],x.shape[3], x.shape[4] )#[:,:,:,0:-1]
+        #breakpoint()
+        # batch size, seq_length
+        return x
 
 
 
---
+++ main_aist_cnn.py
@@ -0,0 +1,227 @@
+import os
+from utils import aist_dataset as datasets
+from torch.utils.data import DataLoader
+from aist_model import *
+import matplotlib.pyplot as plt
+import torch.optim as optim
+import torch.autograd
+import torch
+import numpy as np
+from utils.loss_funcs import *
+from utils.data_utils import define_actions
+from utils.h36_3d_viz import visualize
+from utils.parser import args
+from pdb import set_trace as breakpoint
+from metric import get_metric, get_3d_metric
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print('Using device: %s'%device)
+
+
+model = Model(
+	args.input_dim,args.input_n,
+	args.output_n,args.st_gcnn_dropout,
+	args.joints_to_consider, args.music_dim,
+	args.tcnn_dropout,args.step_size, args.output_step_size,
+	args.music_as_joint, args.n_tcnn_layers, args.tcnn_kernel_size).to(device)
+
+print('total number of parameters of the network is: '+str(sum(p.numel() for p in model.parameters() if p.requires_grad)))
+
+model_name='aist_final_cnn_music_o'+str(args.output_n)+'_i'+str(args.input_n)+'_m'+str(args.music_as_joint)+'_cnn'+str(args.n_tcnn_layers)+'_s'+str(args.step_size)+'_os'+str(args.output_step_size)+'frames_ckpt'
+
+def train():
+
+
+	optimizer=optim.Adam(model.parameters(),lr=args.lr,weight_decay=1e-05)
+
+	if args.use_scheduler:
+			scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=args.milestones, gamma=args.gamma)
+
+	train_loss = []
+	val_loss = []
+	dataset = datasets.Datasets(args.p3d_pth, args.audio_pth,args.input_n,args.output_n,args.skip_rate)
+	print('>>> Training dataset length: {:d}'.format(dataset.__len__()))
+	data_loader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True, num_workers=0, pin_memory=True)
+
+	vald_dataset = datasets.Datasets(args.p3d_val_pth, args.audio_val_pth,args.input_n,args.output_n,args.skip_rate)
+	print('>>> Validation dataset length: {:d}'.format(vald_dataset.__len__()))
+	vald_loader = DataLoader(vald_dataset, batch_size=args.batch_size, shuffle=True, num_workers=0, pin_memory=True)
+
+	dim_used = np.arange(225) # 25 * 9
+	music_dim_used = np.arange(35)
+	# assumed the x y z are 9*1 with 0 pads for each dimension
+
+	curr_train_loss = None
+	curr_val_loss = None
+	curr_metric = None
+
+	for epoch in range(args.n_epochs):
+		running_loss=0
+		n=0
+		model.train()
+		for batch in enumerate(data_loader):
+			cnt, batch = batch
+			p3_batch, music_batch = batch
+			# print(len(batch))
+			p3_batch=p3_batch.to(device)
+			music_batch=music_batch.to(device)
+			batch_dim=p3_batch.shape[0]
+			n+=batch_dim
+			assert p3_batch.shape[2] == 225
+			assert music_batch.shape[2] == 35
+			# print(batch.shape)
+
+			# sequences_train=batch[:, 0:args.input_n, dim_used].view(-1,args.input_n,len(dim_used)//3,3).permute(0,3,1,2)
+			sequences_train=p3_batch[:, 0:args.input_n, dim_used].view(-1,args.input_n,len(dim_used)//args.input_dim,args.input_dim).permute(0,3,1,2)
+			sequences_gt=p3_batch[:, args.input_n:args.input_n+args.output_n, dim_used].view(-1,args.output_n,len(dim_used)//args.input_dim,args.input_dim)
+
+			music_train=music_batch[:, 0:args.input_n, music_dim_used].permute(0,2,1).unsqueeze(3)
+			music_future=music_batch[:, args.input_n:args.input_n+args.output_n, music_dim_used].permute(0,2,1).unsqueeze(3)
+
+			# print(sequences_train.shape, sequences_gt.shape)
+
+
+			optimizer.zero_grad()
+			# change
+			# input sequences_train is of dims N, dim in 1 joint, input_n_frames, joints number
+			# input music_train is N, music_feature_dims, input_time_frames, joints_number_equivalent =1, same for music_future
+			sequences_predict=model(sequences_train, music_train, music_future).permute(0,1,3,2)
+			#print(sequences_predict.shape, sequences_gt.shape)
+			assert (sequences_predict.shape == sequences_gt.shape)
+			loss=mpjpe_error(sequences_predict,sequences_gt)
+
+
+			if cnt % 200 == 0:
+				print('[%d, %5d]  training loss: %.3f' %(epoch + 1, cnt + 1, loss.item()))
+
+			loss.backward()
+			if args.clip_grad is not None:
+				torch.nn.utils.clip_grad_norm_(model.parameters(),args.clip_grad)
+
+			optimizer.step()
+			running_loss += loss*batch_dim
+
+		curr_train_loss = running_loss.detach().cpu()/n
+		train_loss.append(curr_train_loss)
+		model.eval()
+		with torch.no_grad():
+			running_loss=0
+			running_metric=0
+			n=0
+			for batch in enumerate(vald_loader):
+				cnt, batch = batch
+				p3_batch, music_batch = batch
+				p3_batch=p3_batch.to(device)
+				music_batch=music_batch.to(device)
+				batch_dim=p3_batch.shape[0]
+				n+=batch_dim
+
+
+				# sequences_train=batch[:, 0:args.input_n, dim_used].view(-1,args.input_n,len(dim_used)//3,3).permute(0,3,1,2)
+				# sequences_gt=batch[:, args.input_n:args.input_n+args.output_n, dim_used].view(-1,args.output_n,len(dim_used)//3,3)
+				sequences_train=p3_batch[:, 0:args.input_n, dim_used].view(-1,args.input_n,len(dim_used)//args.input_dim,args.input_dim).permute(0,3,1,2)
+				sequences_gt=p3_batch[:, args.input_n:args.input_n+args.output_n, dim_used].view(-1,args.output_n,len(dim_used)//args.input_dim,args.input_dim)
+
+				music_train=music_batch[:, 0:args.input_n, music_dim_used].permute(0,2,1).unsqueeze(3)
+				music_future=music_batch[:, args.input_n:args.input_n+args.output_n, music_dim_used].permute(0,2,1).unsqueeze(3)
+
+				sequences_predict=model(sequences_train, music_train, music_future).permute(0,3,1,2)
+				metric = get_3d_metric(sequences_predict,sequences_gt)
+
+
+				loss=mpjpe_error(sequences_predict,sequences_gt)
+				if cnt % 200 == 0:
+									print('[%d, %5d]  validation loss: %.3f, metric: %.3f' %(epoch + 1, cnt + 1, loss.item(), metric))
+				running_loss+=loss*batch_dim
+				running_metric+=metric*batch_dim
+			curr_val_loss = running_loss.detach().cpu()/n
+			curr_metric = running_metric/n
+			val_loss.append(curr_val_loss)
+		if args.use_scheduler:
+			scheduler.step()
+
+
+		if (epoch+1)%5==0:
+			print('----saving model-----')
+			torch.save(model.state_dict(),os.path.join(args.model_path,model_name+"e%d_v%.3f_t%.3f_m%.3f" % (epoch,curr_val_loss, curr_train_loss, curr_metric)))
+
+
+			plt.figure(1)
+			plt.plot(train_loss, 'r', label='Train loss')
+			plt.plot(val_loss, 'g', label='Val loss')
+			plt.legend()
+			plt.show()
+
+def test():
+
+	model.load_state_dict(torch.load(os.path.join(args.model_path,model_name)))
+	model.eval()
+	accum_loss=0
+	n_batches=0 # number of batches for all the sequences
+	actions=define_actions(args.actions_to_consider)
+
+	dim_used =np.arange(225)
+	music_dim_used =np.arange(35)
+	# joints at same loc
+	# joint_to_ignore = np.array([16, 20, 23, 24, 28, 31])
+	joint_to_ignore = np.array([])
+	index_to_ignore = np.concatenate((joint_to_ignore * 3, joint_to_ignore * 3 + 1, joint_to_ignore * 3 + 2))
+	joint_equal = np.array([13, 19, 22, 13, 27, 30])
+	index_to_equal = np.concatenate((joint_equal * 3, joint_equal * 3 + 1, joint_equal * 3 + 2))
+
+	for action in actions:
+		running_loss=0
+		n=0
+		dataset_test = datasets.Datasets(args.p3d_pth, args.audio_pth,args.input_n,args.output_n,args.skip_rate)
+		print('>>> test action for sequences: {:d}'.format(dataset_test.__len__()))
+
+		test_loader = DataLoader(dataset_test, batch_size=args.batch_size_test, shuffle=False, num_workers=0, pin_memory=True)
+		for batch in enumerate(test_loader):
+			with torch.no_grad():
+
+				cnt, batch = batch
+				p3_batch, music_batch = batch
+				p3_batch=p3_batch.to(device)
+				music_batch=music_batch.to(device)
+				batch_dim=p3_batch.shape[0]
+				n+=batch_dim
+
+
+				all_joints_seq=batch.clone()[:, args.input_n:args.input_n+args.output_n,:]
+
+				# sequences_train=batch[:, 0:args.input_n, dim_used].view(-1,args.input_n,len(dim_used)//3,3).permute(0,3,1,2)
+				# sequences_gt=batch[:, args.input_n:args.input_n+args.output_n, :]
+				sequences_train=p3_batch[:, 0:args.input_n, dim_used].view(-1,args.input_n,len(dim_used)//args.input_dim,args.input_dim).permute(0,3,1,2)
+				sequences_gt=p3_batch[:, args.input_n:args.input_n+args.output_n, dim_used].view(-1,args.output_n,len(dim_used)//args.input_dim,args.input_dim)
+
+				music_train=music_batch[:, 0:args.input_n, music_dim_used].permute(0,2,1).unsqueeze(3)
+				music_future=music_batch[:, args.input_n:args.input_n+args.output_n, music_dim_used].permut(0,2,1).unsqueeze(3)
+
+				sequences_predict=model(sequences_train, music_train, music_future).permute(0,3,1,2).contiguous().view(-1,args.output_n,len(dim_used))
+
+
+
+				all_joints_seq[:,:,dim_used] = sequences_predict
+
+
+				all_joints_seq[:,:,index_to_ignore] = all_joints_seq[:,:,index_to_equal]
+
+				# loss=mpjpe_error(all_joints_seq.view(-1,args.output_n,32,3),sequences_gt.view(-1,args.output_n,32,3))
+				# running_loss+=loss*batch_dim
+				# accum_loss+=loss*batch_dim
+
+		print('loss at test subject for action : '+str(action)+ ' is: '+str(0)) #str(running_loss/n))
+		n_batches+=n
+	#print('overall average loss in mm is: '+str(accum_loss/n_batches))
+
+
+if __name__ == '__main__':
+
+	if args.mode == 'train':
+		train()
+	elif args.mode == 'test':
+		test()
+	elif args.mode=='viz':
+		model.load_state_dict(torch.load(os.path.join(args.model_path,model_name)))
+		model.eval()
+		visualize(args.input_n,args.output_n,args.visualize_from,args.data_dir,model,device,args.n_viz,args.skip_rate,args.actions_to_consider)
